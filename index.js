924e57483c4e45974b6b2c683fa4dab0e61e50f1
Brendan Hack <brendan@migenius.com>
2019-02-24 10:11:27 +1100

    update to new response api

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const path = require('path'),
     fs = require('fs'),
-    { Service, Command } = require('realityserver-client'),
+    { Command,Command_error,Error: Rs_error,Helpers,Service } = require('realityserver-client'),
     WS = require('websocket').w3cwebsocket;
 
+const service = new Service();
 
 require('yargs')
     .demandCommand(7)
@@ -37,7 +38,7 @@ require('yargs')
                     describe: 'output filename, extension defines the file format',
                     type: 'string'
                 });
-        },render)
+        },load_and_render)
     .boolean('ssl')
     .default('ssl',false)
     .describe('ssl','if true connect using wss, otherwise ws')
@@ -45,85 +46,38 @@ require('yargs')
     .alias('h', 'help')
     .argv;
 
-async function render(argv) {
-    const { host,port,ssl,scene_file,width,height,max_samples,filename } = argv;
+function render_scene(scene_info, width, height, max_samples, filename) {
+    return new Promise(async (resolve,reject) => {
+        const camera = scene_info.camera;
+        const options = scene_info.options;
+        const user_scope_name = `scope_${Helpers.create_random_string(8)}`;
 
-    const url = `${(ssl ? 'wss://' : 'ws://')}${host}:${port}/service/`;
-
-    console.log(`connecting to: ${url}`);
-
-    const service = new Service();
-    try {
-        await service.connect(new WS(url,
-            undefined,
-            undefined,
-            { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36' }));
-    } catch (err) {
-        console.error('Web Socket connection failed: ' + err.toString());
-        return;
-    }
-    console.log(`loading scene: ${scene_file}`);
-
-    // service.debug_commands(true);
-    let scene_info;
-    try {
-        const [ response ] = await service.queue_commands()
-            .queue(
-                new Command('create_scope',{ scope_name:'myscope' }))
-            .queue(
-                new Command('use_scope',{ scope_name:'myscope' }))
-            .queue(
-                new Command('import_scene',
-                    {
-                        scene_name:'myscene',
-                        block:true,
-                        filename: scene_file
-                    }),true)
-            .execute();
-
-        if (response.error) {
-            console.error(`scene load failed: ${JSON.stringify(response.error)}`);
-            service.close();
-            return;
-        }
-        scene_info = response.result;
-    } catch(err) {
-        console.error('Service error loading scene: ' + err.toString());
-        service.close();
-        return;
-    }
-    console.log(`rendering at ${width}x${height} for ${max_samples} iterations`);
-
-    const camera = scene_info.camera;
-    const options = scene_info.options;
-
-    let image;
-    try {
-        const [ response ] = await service.queue_commands()
-            .queue(
-                new Command('use_scope',{ scope_name:'myscope' })
-            )
+        const [ image ] = await service.queue_commands()
+            .queue(new Command('create_scope', { scope_name: user_scope_name, parent_scope: scene_info.scope_name }))
+            .queue(new Command('use_scope', { scope_name: user_scope_name }))
+            .queue(new Command('localize_element',{ element_name: options }))
+            .queue(new Command('localize_element',{ element_name: camera }))
             .queue(new Command('camera_set_resolution',{
-                camera_name:camera,
+                camera_name: camera,
                 resolution: {
-                    x:parseInt(width,10),
-                    y:parseInt(height,10)
+                    x: width,
+                    y: height
                 }
             }))
             .queue(new Command('camera_set_aspect',{
-                camera_name:camera,
-                aspect: parseFloat(width,10) / parseFloat(height,10)
+                camera_name: camera,
+                aspect: width / height
             }))
-            .queue(new Command('element_set_attribute',{
-                element_name:options,
+            .queue(new Command('element_set_attribute', {
+                element_name: options,
                 attribute_name: 'progressive_rendering_max_samples',
-                attribute_value: parseInt(max_samples,10),
+                attribute_value: max_samples,
                 attribute_type: 'Sint32'
             }))
             .queue(new Command('render',
                 {
-                    scene_name:'myscene',
-                    renderer:'iray',
+                    scene_name: scene_info.scene_name,
+                    renderer: 'iray',
                     format: path.extname(filename).slice(1),
                     render_context_options: {
                         scheduler_mode: {
@@ -132,30 +86,83 @@ async function render(argv) {
                         }
                     }
                 }),true)
-            .execute();
+            .queue(new Command('delete_scope', { scope_name: user_scope_name }))
+            .execute().catch(err => [err]);
 
-        if (response.error) {
-            console.error(`render error: ${JSON.stringify(response.error)}`);
-            service.close();
+        if (image instanceof Command_error) {
+            reject(`render error: ${image.message}`);
             return;
-        }  
-        image = response.result;  
-    } catch(err) {
-        console.error('Service error rendering scene: ' + err.toString());
+        }
+        if (image instanceof Rs_error) {
+            reject(`render error: ${image.toString()}`);
+            return;
+        }
+
+        if (!image.data) {
+            reject('no rendered image');
+            return;
+        }
+        fs.writeFile(filename,image.data,(err) => {
+            if (err) {
+                reject(`error writing file ${err}`);
+            } else {
+                resolve(`image saved to ${filename}`);
+            }
+        });
+    });
+}
+
+async function load_and_render(argv) {
+    const { host,port,ssl,scene_file,width,height,max_samples,filename } = argv;
+
+    const url = `${(ssl ? 'wss://' : 'ws://')}${host}:${port}/service/`;
+
+    console.log(`connecting to: ${url}`);
+
+    try {
+        await service.connect(new WS(url,
+            undefined,
+            undefined,
+            { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36' }));
+    } catch (err) {
+        console.error(`Web Socket connection failed: ${err.toString()}`);
+        return;
+    }
+    console.log(`loading scene: ${scene_file}`);
+
+    // service.debug_commands(true);
+    const [ scene_info ] = await service.queue_commands()
+        .queue(new Command('create_scope',{ scope_name: filename }))
+        .queue(new Command('use_scope',{ scope_name: filename }))
+        .queue(
+            new Command('import_scene',
+                {
+                    scene_name: 'myscene',
+                    block: true,
+                    filename: scene_file
+                }),true)
+        .execute().catch(err => [ err ]);
+
+    if (scene_info instanceof Command_error) {
+        console.error(`scene load failed: ${scene_info.message}`);
         service.close();
         return;
     }
-    service.close();
-
-    if (!image.data) {
-        console.error('no rendered image');
+    if (scene_info instanceof Rs_error) {
+        console.error('Service error loading scene: ' + scene_info.toString());
+        service.close();
         return;
     }
-    fs.writeFile(filename,image.data,(err) => {
-        if (err) {
-            console.error('error writing file ' + err);
-        } else {
-            console.log(`image saved to ${filename}`);
-        }
-    });
+
+    console.log(`rendering at ${width}x${height} for ${max_samples} iterations`);
+
+    try {
+        scene_info.scene_name = 'myscene';
+        scene_info.scope_name = filename;
+        await render_scene(scene_info,parseInt(width,10),parseInt(height,10),parseInt(max_samples,10),filename);
+        console.log(`image saved to ${filename}`);
+    } catch (err) {
+        console.error(err);
+    }
+    service.close();
 }
